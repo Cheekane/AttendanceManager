29e80d7c73405c2b78d5ac359f28d678da473027
Brian <brianpark1101@gmail.com>
2023-12-30 15:56:20 -0500

    added search bar

---
+++ client/src/components/SearchResults.jsx
@@ -0,0 +1,10 @@
+import React, { useState, useEffect } from "react"
+import axios from "axios"
+
+const SearchResults = () => {
+    const [results, setResults] = useState()
+    
+    
+}
+
+export default SearchResults
