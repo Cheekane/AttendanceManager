ee286140bfd2c6b136b5601a8285a3f69b96a078
Brian <brianpark1101@gmail.com>
2024-01-12 16:40:08 -0500

    updated searchbar

--- client/src/components/SearchResults.jsx
+++ client/src/components/SearchResults.jsx
@@ -1,11 +1,16 @@
 import React, { useState, useEffect } from "react"
+import { useNavigate } from "react-router-dom"
 import "./SearchResults.scss"
 
 const SearchResults = ({results}) => {
+    const navigate = useNavigate()
+    const handleClick = (id) => {
+        navigate(`/memberInfo/${id}`)
+    }
     return (
         <div className="results-list"> 
             {results.map((result, id) => {
-                return <div key={id} className="result-fullname">{result.firstname} {result.lastname}</div>
+                return <div key={id} className="result-fullname" onClick={() => handleClick(result.id)}>{result.firstname} {result.lastname}</div>
             })}
         </div>
     )
