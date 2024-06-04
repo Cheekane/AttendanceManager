6dfd4284cae56049c02c53f6fe55d88d91404ad3
Brian <brianpark1101@gmail.com>
2024-06-04 00:35:20 -0400

    cleaned up Home page

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -4,36 +4,6 @@ import { useNavigate } from "react-router-dom"
 import "./styles/Home.scss"
 
 const Home = () => {
-  const [members, setMembers] = useState([])
-  const navigate = useNavigate()
-
-  const [results, setResults] = useState([])
-
-  // useEffect(() => {
-  //   const fetchAllMembers = async () => {
-  //     try {
-  //       const res = await axios.get("http://localhost:8800/members")
-  //       setMembers(res.data)
-  //     } catch (err) {
-  //       console.log(err)
-  //     }
-  //   }
-  //   fetchAllMembers()
-  // }, [])
-
-  // const handleDelete = async (id) => {
-  //   try {
-  //     await axios.delete(`http://localhost:8800/member/${id}`)
-  //     setMembers((prevMembers) => prevMembers.filter((member) => member.id !== id))
-  //   } catch (err) {
-  //     console.log(err)
-  //   }
-  // }
-
-  const handleClickInfo = (id) => { // handles clicks on info buttons
-    navigate(`/memberInfo/${id}`)
-  }
-
 
   return (
     <div className="main-container">
